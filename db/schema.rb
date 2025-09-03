46d7a7f8d476ab8dd8d5891567eed9fe1fbe9a0e
cbeery <git@curtbeery.com>
2025-09-03 12:25:22 -0600

    Adds omniauth fields to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_02_220007) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_03_182506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -173,6 +173,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_02_220007) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "admin", default: false, null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

39e5f63ee9754b95f113d67b97b039ac68c538fd
cbeery <git@curtbeery.com>
2025-09-03 12:24:46 -0600

    Updates some Devise config for Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :name, presence: true, length: { minimum: 2, maximum: 100 }
   
@@ -8,4 +9,16 @@ class User < ApplicationRecord
   def display_name
     name.presence || email.split('@').first
   end
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
+
 end

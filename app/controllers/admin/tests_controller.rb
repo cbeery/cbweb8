644dba6e7e3eef34f25664130a07751cb160a58a
cbeery <git@curtbeery.com>
2025-09-04 09:47:17 -0600

    Adds admin controllers

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,7 +1,28 @@
-class Admin::TestsController < ApplicationController
+class Admin::TestsController < Admin::BaseController
   def s3_upload
+    @upload = Upload.new
+  end
+
+  def s3_create
+    @upload = Upload.new(upload_params)
+    if @upload.save
+      redirect_to admin_test_s3_path, notice: "File uploaded successfully!"
+    else
+      render :s3_upload
+    end
   end
 
   def active_job
+  end
+
+  def trigger_job
+    TestJob.perform_later(params[:message] || "Test message")
+    redirect_to admin_test_job_path, notice: "Job queued successfully!"
+  end
+
+  private
+
+  def upload_params
+    params.require(:upload).permit(:file, :title)
   end
 end

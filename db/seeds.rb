e72dab45764b9ff9c953d6b974617a2d36993f53
cbeery <git@curtbeery.com>
2025-09-26 11:58:34 -0600

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Initial user
+User.create!(name: 'Curt Beery', email: 'cb@curtbeery.com', admin: true)

9b25f19659e6e8442f8fcae1f33ef211053cc80e
cbeery <git@curtbeery.com>
2025-10-03 12:34:20 -0600

    Adds release date fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_182028) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_03_183332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -319,10 +319,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_182028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "album_image_url"
+    t.date "release_date"
+    t.string "release_date_precision"
+    t.integer "release_year"
     t.index ["album"], name: "index_spotify_tracks_on_album"
     t.index ["album_image_url"], name: "index_spotify_tracks_on_album_image_url"
     t.index ["artist_sort_text"], name: "index_spotify_tracks_on_artist_sort_text"
     t.index ["popularity"], name: "index_spotify_tracks_on_popularity"
+    t.index ["release_date"], name: "index_spotify_tracks_on_release_date"
+    t.index ["release_year"], name: "index_spotify_tracks_on_release_year"
     t.index ["spotify_id"], name: "index_spotify_tracks_on_spotify_id", unique: true
   end
 

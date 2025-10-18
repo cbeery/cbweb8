eec3cf1e1b62fbb386129714b498600119d29c4e
cbeery <git@curtbeery.com>
2025-10-18 11:19:25 -0600

    Updates game nav

--- app/javascript/controllers/date_navigator_controller.js
+++ app/javascript/controllers/date_navigator_controller.js
@@ -1,12 +1,18 @@
 // app/javascript/controllers/date_navigator_controller.js
 import { Controller } from "@hotwired/stimulus"
-import { visit } from "@hotwired/turbo"
 
 export default class extends Controller {
+  connect() {
+    console.log("Date navigator connected to:", this.element)
+  }
+  
   navigate(event) {
     const date = event.target.value
+    console.log("Navigating to date:", date)
+    
     if (date) {
-      visit(`/admin/nba/games?date=${date}`)
+      // Simple navigation - just change the URL
+      window.location.href = `/admin/nba/games?date=${date}`
     }
   }
 }

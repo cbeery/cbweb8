c820435fc56eddb71c31b020fa75287b6bbf8e8f
cbeery <git@curtbeery.com>
2025-11-10 12:50:14 -0700

    Adds film series and viewing associations

--- app/models/film_series.rb
+++ app/models/film_series.rb
@@ -1,2 +1,4 @@
 class FilmSeries < ApplicationRecord
+  has_many :film_series_events, -> { order('started_on DESC')}, dependent: :destroy
+  has_many :viewings, -> { order('viewed_on DESC')}, through: :film_series_events
 end
--- app/models/film_series_event.rb
+++ app/models/film_series_event.rb
@@ -1,2 +1,4 @@
 class FilmSeriesEvent < ApplicationRecord
+  belongs_to :film_series
+  has_many :viewings, -> { order('viewed_on DESC')}
 end
